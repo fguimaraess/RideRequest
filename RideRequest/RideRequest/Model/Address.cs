0d805b1b32c96ad83dd4c640ea1b0b08271655ab
Felipe Guimarães <felipe.guimaraes@elevaeducacao.com.br>
2019-02-25 09:02:09 -0300

    starting app

---
+++ RideRequest/RideRequest/Model/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RideRequest.Model
+{
+    public class Address
+    {
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ RideRequest/RideRequest/Model/Places.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RideRequest.Model
+{
+    public class Suggestions
+    {
+        public List<Suggestion> Suggestion { get; set; }
+    }
+
+    public class Suggestion
+    {
+        public string Label { get; set; }
+        public string Language { get; set; }
+        public string CountryCode { get; set; }
+        public string LocationId { get; set; }
+        public Address Address { get; set; }
+        public string MatchLevel { get; set; }
+    }
+
+    public class Place
+    {
+        public string label { get; set; }
+        public string language { get; set; }
+        public string countryCode { get; set; }
+        public string locationId { get; set; }
+        //public Address Address { get; set; }
+        public string matchLevel { get; set; }
+    }
+}
